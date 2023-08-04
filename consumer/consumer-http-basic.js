a8c72dbc8b22ee0dc3fb4719bb1371c1d0af7cb8
brameldering <bram.eldering@gmail.com>
2023-08-04 11:42:54 +0200

    Consolidated project - HAProxy TLS

--- consumer/consumer-http-basic.js
+++ consumer/consumer-http-basic.js
@@ -1,13 +1,11 @@
 #!/usr/bin/env node
 // npm install fastify@3.2 node-fetch@2.6
-// Warning: Not as efficient as using a Reverse Proxy
 const server = require("fastify")();
 const fetch = require("node-fetch");
-const https = require("https");
-const fs = require("fs");
 const HOST = "127.0.0.1";
 const PORT = process.env.PORT || 3000;
 const TARGET = process.env.TARGET || "127.0.0.1:4000";
+
 server.get("/", async () => {
   const req = await fetch(`http://${TARGET}/recipes/42`);
   const payload = await req.json();
